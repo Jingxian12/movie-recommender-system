de420650985256fd61869c3881d201733f09209a
Jingxian12 <152453399+Jingxian12@users.noreply.github.com>
2026-06-01 11:58:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st 
 
+st.set_page_config(
+    page_title="Hybrid Movie Recommender",
+    layout="wide"
+)
+
+st.title("🎬 Hybrid Movie Recommender System")
+
+mode = st.sidebar.radio(
+    "Select Recommendation Mode",
+    [
+        "Personalized Recommendation",
+        "Similar Movie Search",
+        "Semantic Search"
+    ]
+)
