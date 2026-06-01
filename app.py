405e64c1c4a1b3961e60a5bfe2d01e9e1d155471
Jingxian12 <152453399+Jingxian12@users.noreply.github.com>
2026-06-01 22:17:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 # =========================
 # PAGE CONFIG
 # =========================
-st.set_page_config(page_title="Hybrid Movie Recommender", layout="wide")
+st.set_page_config(page_title="MovieMatch", layout="wide")
 
 st.title("🎬 Hybrid Movie Recommender System")
-st.write("Welcome to MovieMatch! Log in with your User ID to unlock personalized recommendations based on your unique rating history, or jump straight into Guest Mode to explore movies by matching your favorite vibes and descriptions")
+st.write("Welcome to MovieMatch! Log in with your User ID to unlock personalized recommendations based on your unique rating history, or jump straight into Guest Mode to explore movies by matching your favorite vibes and descriptions.")
 st.divider()
 
 # =========================
@@ -23,7 +23,7 @@ ratings = pd.read_csv("dataset/ratings_clean.csv")
 links = pd.read_csv("dataset/movieLens.csv")
 
 # =========================
-# BUILD MAPPING (CRITICAL FIX)
+# BUILD MAPPING 
 # =========================
 ratings = ratings.merge(links[["movieId", "tmdbId"]], on="movieId")
 ratings = ratings.merge(movies, on="tmdbId")
@@ -40,7 +40,7 @@ user_item_matrix = ratings.pivot_table(
 # =========================
 # LOAD MODELS
 # =========================
-# CF
+# ITEM SIMILIARITY
 with gzip.open("models/cf/item_topk.pkl.gz", "rb") as f:
     item_topk = pickle.load(f)
 
@@ -51,29 +51,6 @@ with gzip.open("models/content/tfidf_topk.pkl.gz", "rb") as f:
 # SBERT embeddings
 with gzip.open("models/nlp/sbert_embeddings.pkl.gz", "rb") as f:
     embeddings = pickle.load(f)
-
-# =========================
-# CF FUNCTION
-# =========================
-def recommend_cf(user_id):
-    if user_id not in user_item_matrix.index:
-        return movies.sample(10)
-
-    user_ratings = user_item_matrix.loc[user_id]
-    watched = user_ratings[user_ratings > 0].index
-
-    scores = {}
-
-    for movie in watched:
-        if movie in item_topk:
-            for neighbor, sim in item_topk[movie]:
-                scores[neighbor] = scores.get(neighbor, 0) + sim
-
-    ranked = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-
-    rec_tmdb = [i[0] for i in ranked[:10]]
-
-    return movies[movies["tmdbId"].isin(rec_tmdb)]
 
 # =========================
 # MOVIE DETAILS UI
@@ -95,6 +72,29 @@ def show_movie(movie):
         st.write("**Rating:**", movie["vote_average"])
         st.write("**Runtime:**", movie["runtime"])
         st.write("**Release Date:**", movie["release_date"])
+        
+# =========================
+# CF FUNCTION
+# =========================
+def recommend_cf(user_id):
+    # Fallback Mechanism
+    if user_id not in user_item_matrix.index:
+        return movies.sample(5)
+
+    user_ratings = user_item_matrix.loc[user_id]
+    watched = user_ratings[user_ratings > 0].index
+    scores = {}
+    for movie in watched:
+        if movie in item_topk:
+            for neighbor, sim in item_topk[movie]:
+                scores[neighbor] = scores.get(neighbor, 0) + sim
+
+    ranked = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+    rec_tmdb = [i[0] for i in ranked[:5]]
+
+    return movies[movies["tmdbId"].isin(rec_tmdb)]
+
 
 # =========================
 # LOGOUT FUNCTION
@@ -116,11 +116,8 @@ if "mode" not in st.session_state:
 # HOME
 # =========================
 if st.session_state.mode == "home":
-
     st.subheader("Choose Mode")
-
     col1, col2 = st.columns(2)
-
     with col1:
         if st.button("🔐 Login"):
             st.session_state.mode = "login"
@@ -133,26 +130,27 @@ if st.session_state.mode == "home":
 # LOGIN
 # =========================
 elif st.session_state.mode == "login":
+    col1, col2 = st.columns([1, 6])
+    with col1:
+        if st.button("⬅️ Home"):
+            st.session_state.mode = "home"
+            st.rerun()
 
     st.subheader("Enter User ID")
-
     uid = st.text_input("User ID")
-
     if st.button("Submit"):
-
         if uid.isdigit() and int(uid) in user_item_matrix.index:
             st.session_state.user_id = int(uid)
             st.session_state.mode = "user"
+            st.rerun()
         else:
-            st.error("User not found")
-
+            st.error("❌ User not found. Please try again by enter validID!")
+            
 # =========================
 # USER MODE
 # =========================
 elif st.session_state.mode == "user":
-
     col1, col2 = st.columns([8, 1])
-
     with col2:
         if st.button("🚪 Sign Out"):
             logout()
@@ -169,22 +167,16 @@ elif st.session_state.mode == "user":
     # CF TAB
     # =========================
     with tab1:
-
         st.header("Personalized Recommendations")
-
         recs = recommend_cf(st.session_state.user_id).reset_index(drop=True)
-
         selected = None
         cols = st.columns(5)
 
         for i, row in recs.iterrows():
-
             with cols[i % 5]:
                 st.image(row["poster_url"], use_container_width=True)
-
                 if st.button(row["title"], key=f"cf_{i}"):
                     selected = row
-
         if selected is not None:
             st.divider()
             show_movie(selected)
@@ -199,9 +191,7 @@ elif st.session_state.mode == "user":
         movie = st.selectbox("Select Movie", movies["title"])
 
         if st.button("Recommend"):
-
-            tmdb_id = movies[movies["title"] == movie]["tmdbId"].values[0]
-
+            tmdb_id = int(movies[movies["title"] == movie]["tmdbId"].values[0])
             # ✅ FIX: USE tmdbId (NOT index)
             if str(tmdb_id) not in content_topk:
                 st.error("No recommendations found")
