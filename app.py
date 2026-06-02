eef49ce06476ad2c74bfb846e6c3edb7ff4c6d8b
Jingxian12 <152453399+Jingxian12@users.noreply.github.com>
2026-06-02 18:23:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ elif st.session_state.mode == "user":
     col1, col2 = st.columns([8, 1])
     with col1:
         if st.button("🙍🏻‍♂️ Login Page"):
-            st.session_state.mode = "home"
+            st.session_state.mode = "login"
             st.rerun()
     with col2:
         if st.button("🚪 Sign Out", key="user_logout"):
